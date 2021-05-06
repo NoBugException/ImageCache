b41f947acf54316c8432dd1855360e13b24ee2b0
zhouyunchong <943174533@qq.com>
2021-05-06 23:31:06 +0800

    inBitmap封装

--- app/src/main/java/com/example/imagecache/MainActivity.java
+++ app/src/main/java/com/example/imagecache/MainActivity.java
@@ -1,14 +1,64 @@
 package com.example.imagecache;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.collection.LruCache;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+    private Button button;
+
+    private Bitmap bitmap= null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.iv_image);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inSampleSize = 2;
+                options.inMutable = true;
+                bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.kongque, options);
+                options.inBitmap = bitmap;
+                bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ceshi, options);
+                imageView.setImageBitmap(bitmap);
+            }
+        });
+    }
+
+    /**
+     * 得到bitmap的大小
+     */
+    private int getBitmapSize(Bitmap bitmap) {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {    //API 19
+            return bitmap.getAllocationByteCount();
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR1) {//API 12
+            return bitmap.getByteCount();
+        }
+        // 在低版本中用一行的字节x高度
+        return bitmap.getRowBytes() * bitmap.getHeight();
     }
 }
